5d3dc18769ca88d968d8fc9ace111b45c7babfbb
Fabi0o <marfab32@gmail.com>
2022-03-04 19:18:52 +0100

    code refactor

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import Logo from "./logo.png";
 import { createHeader } from "./header.js";
 import { createNav } from "./nav";
 import { createHome } from "./home";
+import { createMenu } from "./menu";
 const content = document.querySelector("#content");
 content.append(createHeader(Logo), createNav(), createHome());
