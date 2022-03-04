7072a6751530445c62a6cdc9bcb147acd32cda0c
Fabi0o <marfab32@gmail.com>
2022-03-04 20:29:31 +0100

    add button functionalities

--- src/index.js
+++ src/index.js
@@ -6,3 +6,23 @@ import { createHome } from "./home";
 import { createMenu } from "./menu";
 const content = document.querySelector("#content");
 content.append(createHeader(Logo), createNav(), createHome());
+
+const homeBtn = document.querySelector(".homeBtn");
+const menuBtn = document.querySelector(".menuBtn");
+const contactBtn = document.querySelector(".contactBtn");
+
+homeBtn.addEventListener("click", () => {
+  const mainContent = document.querySelector(".mainContent");
+  mainContent.remove();
+  content.append(createHome());
+});
+
+menuBtn.addEventListener("click", () => {
+  const mainContent = document.querySelector(".mainContent");
+  mainContent.remove();
+  content.append(createMenu());
+});
+
+contactBtn.addEventListener("click", () => {
+  alert("WIP");
+});
